d518c00874577268581467652f97e08fd25c0cbb
soul059 <keval059@outlook.com>
2024-05-29 08:47:28 +0530

    edit todo and check mark done

--- reduxtest/src/features/Todo/todoSlice.js
+++ reduxtest/src/features/Todo/todoSlice.js
@@ -27,10 +27,18 @@ export const todoSlice = createSlice({
         },
         deleteTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload)
-        }
+        },
+        editTodo: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload.id)
+            
+            
+            if(todo){
+                todo.text = action.payload.text
+            }
+        },
     }
 })
 
-export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions
+export const { addTodo, toggleTodo, deleteTodo , editTodo } = todoSlice.actions
 
 export default todoSlice.reducer
